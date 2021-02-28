75f00403074410fd137c7c1e578c6465b7556415
LawrieDrew <laurenn.drew@gmail.com>
2021-02-27 19:00:13 -0500

    fixed up read me, having trouble connecting java script to hmtl pages, logic now not responsive

--- README.md
+++ README.md
@@ -14,13 +14,21 @@ It runs in the browser with scores saved on local storage (I know, I'm also impr
 
 Behold! Quizzasaurus! 
 
-![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/04-web-apis-homework-demo.gif)
+##
 
-![Code Quiz Main]()
+![portfolio demo](./Assets/04-web-apis-homework-demo.gif)
+
+
+![homepage](C:\Users\Laure\Code_Quiz-\images\Code_Quiz.jpg)
 
 Here is the deployed website URL:
 
-![Github URL]()
+![Github URL]() 
+
+
+![Code Quiz Main](https://github.com/LawrieDrew/Code_Quiz-.git)
+
+
 
 ---
 
@@ -44,82 +52,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-## User Story
-
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
-```
-
-## Mock-Up
-
-The following animation demonstrates the application functionality:
-
-
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
----
-
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
--- script.js
+++ script.js
@@ -3,8 +3,57 @@ const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-
+const scoreText = document.getElementById('score')
+const timeText = document.getElementById('timer')
 let shuffledQuestions, currentQuestionIndex
+
+const questions = [
+    {
+        question: 'What is a sprocket?',
+        answers: [
+            { text: 'Ya Moms', correct: true },
+            {text: 'A Rocket', correct: false},
+            {text: 'You made that up', correct: true},
+            {text: 'Yeah, its not a thing', correct: true}
+        ]
+    },
+    {
+        question: 'Whats my name?',
+        answers: [
+                { text: 'That Bitch', correct: true},
+                { text: 'Fab-Bitch', correct: true},
+                { text: 'Laurie', correct: true},
+                { text: 'Lauren', correct: false}
+                ]
+            },
+            {
+                question: 'Should you ever cook bacon naked?',
+                answers: [
+                        { text: 'I mean', correct: false},
+                        { text: 'Yeah do it', correct: false},
+                        { text: 'Never', correct: true},
+                        { text: 'Lets make eggs too!', correct: false}
+                        ]
+                    },
+                     {
+                        question: 'Whats is my dogs name?',
+                        answers: [
+                                { text: 'Chewy', correct: true},
+                                { text: 'Pookie', correct: true},
+                                { text: 'Pookeets', correct: true},
+                                { text: 'Rod Stewart', correct: true}
+                                ]
+                            },
+                            {
+                                question: 'Whats is my dogs name?',
+                                answers: [
+                                        { text: 'Chewy', correct: true},
+                                        { text: 'Pookie', correct: true},
+                                        { text: 'Pookeets', correct: true},
+                                        { text: 'Rod Stewart', correct: true}
+                                        ]
+                                    },
+    ]
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
@@ -18,13 +67,28 @@ function startGame() {
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
+    startTimer();
     setNextQuestion()
-}
+};
+
+function startTimer() {
+    timer = setInterval(function() {
+        timer--;
+        secondsLeft.textContent = timerText;
+        if (timer === 0 || questionCount > questions.length) {
+            clearInterval(timer);
+            nextButton();
+        };
+        if (timerCount <= 10) {
+            secondsLeft.style.color = "#ff8f8f";
+        };
+    },1000);
+};
 
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+};
 
 function showQuestion(question) {
     questionElement.innerText = question.question
@@ -38,17 +102,14 @@ function showQuestion(question) {
         button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
     })
-}
-
+};
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
         (answerButtonsElement.firstChild)
-    }
-}
-
+    };
 
 function selectAnswer(e) {
     const selectedButton = e.target
@@ -57,75 +118,23 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (shuffledQuestions.length > currentQuestionIndex + 1 ) {
-        nextButton.classList.remove('hide')
+if (shuffledQuestions.length > currentQuestionIndex + 1 ) {
+    nextButton.classList.remove('hide')
+} else {
+    startButton.innerText = 'restart'
+    startButton.classList.remove('hide')
+}
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
     } else {
-        startButton.innerText = 'Restart'
-        startButton.classList.remove('hide')
+        element.classList.add('wrong')
+        }
+    
     }
     
-}
-
-function setStatusClass(element, correct){
-clearStatusClass(element)
-if (correct) {
-    element.classList.add('correct')
-} else {
-    element.classList.add('wrong')
+    function clearStatusClass(element) {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
     }
-
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
-const questions = [
-{
-    question: 'What is a sprocket?',
-    answers: [
-        { text: 'Ya Moms', correct: true },
-        {text: 'A Rocket', correct: false},
-        {text: 'You made that up', correct: true},
-        {text: 'Yeah, its not a thing', correct: true}
-    ]
-},
-{
-    question: 'Whats my name?',
-    answers: [
-            { text: 'That Bitch', correct: true},
-            { text: 'Fab-Bitch', correct: true},
-            { text: 'Laurie', correct: true},
-            { text: 'Lauren', correct: false}
-            ]
-        },
-        {
-            question: 'Should you ever cook bacon naked?',
-            answers: [
-                    { text: 'I mean', correct: false},
-                    { text: 'Yeah do it', correct: false},
-                    { text: 'Never', correct: true},
-                    { text: 'Lets make eggs too!', correct: false}
-                    ]
-                },
-                 {
-                    question: 'Whats is my dogs name?',
-                    answers: [
-                            { text: 'Chewy', correct: true},
-                            { text: 'Pookie', correct: true},
-                            { text: 'Pookeets', correct: true},
-                            { text: 'Rod Stewart', correct: true}
-                            ]
-                        },
-                        {
-                            question: 'Whats is my dogs name?',
-                            answers: [
-                                    { text: 'Chewy', correct: true},
-                                    { text: 'Pookie', correct: true},
-                                    { text: 'Pookeets', correct: true},
-                                    { text: 'Rod Stewart', correct: true}
-                                    ]
-                                },
-]
-
